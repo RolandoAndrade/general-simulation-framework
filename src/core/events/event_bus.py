d36318d92ae463786b39f358e144409788ec2d49
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-02-14 12:25:20 -0400

    Emit

--- src/core/events/event_bus.py
+++ src/core/events/event_bus.py
@@ -11,17 +11,24 @@ class EventBus:
     def __init__(self):
         self._events = {}
 
+    "Subscribe an object to event."
     def subscribe(self, event: str, subscriber: Subscriber):
         if event in self._events:
             self._events[event].append(subscriber)
         else:
             self._events[event] = [subscriber]
 
+    "Unsubscribe an object to event."
     def unsubscribe(self, event: str, subscriber: Subscriber):
         if event in self._events:
             self._events[event].remove(subscriber)
+        else:
+            raise Exception("Event is not defined")
 
+    "Send a message to all subscribers for an event"
     def emit(self, event: str, message: Any):
         if event in self._events:
             for e in self._events[event]:
-                e.on_message_received(message)
+                e.on_message_received(event, message)
+        else:
+            raise Exception("Event is not defined")
--- src/core/events/subscriber.py
+++ src/core/events/subscriber.py
@@ -7,23 +7,31 @@ if TYPE_CHECKING:
 
 class Subscriber:
     _event_bus: EventBus
-    _callbacks: Dict[str, Callable[[Any], None]]
+    _callbacks: Dict[str, Callable[[Any], Any]]
 
     def __init__(self, event_bus: EventBus):
         self._event_bus = event_bus
         self._callbacks = {}
 
+    "Subscribe to event messages"
     def subscribe(self, event: str):
         self._event_bus.subscribe(event, self)
 
+    "Unsubscribe to event messages"
     def unsubscribe(self, event: str):
         self._event_bus.unsubscribe(event, self)
 
+    "Call back methods defined for messages received from event bus"
     def on_message_received(self, event: str, message: Any):
         if event in self._callbacks:
             self._callbacks[event](message)
 
-    def on(self, event: str, function: Callable[[Any], None]):
+    "Listen for messages"
+    def on(self, event: str, function: Callable[[Any], Any]):
         if not(event in self._callbacks):
             self.subscribe(event)
-            self._callbacks[event] = function
+        self._callbacks[event] = function
+
+    "Send a message to all subscribers for an event"
+    def emit(self, event: str, message: Any):
+        self._event_bus.emit(event, message)
