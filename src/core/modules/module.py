b8d2d5f1b718efdc3003468374a7949ae9dfddf6
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-02-14 18:14:18 -0400

    Starting module metadata

--- src/core/modules/module.py
+++ src/core/modules/module.py
@@ -7,7 +7,25 @@ if TYPE_CHECKING:
 
 
 class Module:
-    providers: List[Provider]
+    providers: List[type]
+    exports: List[type]
     imports: List[Module]
     dependencies: List[Provider]
-    exports: List[Provider]
+    instances: List[Provider]
+
+    def _find_dependency(self, dependency: type)->Provider:
+        for dep in self.dependencies:
+            if type(dep) == dependency:
+                return dep
+        raise Exception("Missing dependency " + str(dependency))
+
+    def get_exported_dependencies(self)->List[Provider]:
+        dependencies: List[Provider] = []
+        for provider in self.instances:
+            if type(provider) in self.exports:
+                dependencies.append(provider)
+        return dependencies
+
+    def _inject_dependencies(self):
+        for provider in self.providers:
+            provider.__init__()
