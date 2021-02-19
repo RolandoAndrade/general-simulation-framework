d8f037d0a19d774282947b173a3f094fb0a52eab
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-02-19 18:00:28 -0400

    Inject components

--- src/core/modules/base_module.py
+++ src/core/modules/base_module.py
@@ -1,5 +1,9 @@
+import logging
 from typing import List
 
+from injector import inject, Injector, Module, provider
 
-class BaseModule:
-    pass
+class BaseModule(Module):
+
+    def __init__(self):
+        print("Init module " + str(self.__class__.__name__))
