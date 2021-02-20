8d6f0358ffe9b4186476713b69ea2c4fca7869e2
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-02-20 15:13:14 -0400

    Abstract atomic model

--- src/dynamic_system/atomic_model/atomic_model.py
+++ src/dynamic_system/atomic_model/atomic_model.py
@@ -1,21 +1,36 @@
+from __future__ import annotations
+from abc import abstractmethod
 from typing import Set, TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from dynamic_system.atomic_model.variable import Variable
+    from dynamic_system.atomic_model.value import Value
 
 from dynamic_system.base_model import BaseModel
 
 
 class AtomicModel(BaseModel):
+    _variables: Set[Value]
 
-    _variables: Set[Variable]
+    """Implements the state transition function
+    :param bag_xb: set of bags with elements in X (inputs set)
+    """
 
-    """Implements the state transition function"""
-
-    def delta(self):
+    @abstractmethod
+    def delta(self, bag_xb: Set[Value]):
         pass
 
-    """Implements the output function"""
+    """Implements the output function
+    :param bag_yb: set of bags with elements in Y (outputs set)
+    """
 
-    def output_function(self):
+    @abstractmethod
+    def output_function(self, bag_yb: Set[Value]):
+        pass
+
+    """Clear the allocated objects used for """
+
+    @abstractmethod
+    def garbage_collector_output(self):
+        """"This is necessary because the model that created the object cannot know
+        when the simulator is finished with it."""
         pass
