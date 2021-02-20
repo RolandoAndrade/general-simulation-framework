2408e3beb09bc1fa873e8b89d92f9ee8dffab212
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-02-20 14:50:51 -0400

    Abstract atomic model

--- src/dynamic_system/atomic_model/atomic_model.py
+++ src/dynamic_system/atomic_model/atomic_model.py
@@ -1,5 +1,21 @@
+from typing import Set, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from dynamic_system.atomic_model.variable import Variable
+
 from dynamic_system.base_model import BaseModel
 
 
 class AtomicModel(BaseModel):
-    pass
+
+    _variables: Set[Variable]
+
+    """Implements the state transition function"""
+
+    def delta(self):
+        pass
+
+    """Implements the output function"""
+
+    def output_function(self):
+        pass
