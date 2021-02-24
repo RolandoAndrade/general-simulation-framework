7c6fba2e640db851caeb5a17542d7965e784de78
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-02-24 19:45:31 -0400

    Input bag

--- src/dynamic_system/base_model.py
+++ src/dynamic_system/base_model.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, List
 
+
 if TYPE_CHECKING:
     from dynamic_system.atomic_models.bag_of_values import BagOfValues
+    from dynamic_system.input_bag import InputBag
 
 from abc import abstractmethod
 
@@ -16,3 +18,7 @@ class BaseModel:
 
     def _remove_listener(self, model: BaseModel):
         self._listeners.remove(model)
+
+    @abstractmethod
+    def receive_input(self, input_bag: InputBag):
+        pass
--- src/dynamic_system/input_model.py
+++ src/dynamic_system/input_model.py
@@ -5,9 +5,11 @@ from typing import TYPE_CHECKING
 from core.events.event_bus import subscriber, subscribe
 from dynamic_system.base_model import BaseModel
 from dynamic_system.events.compute_output_event import ComputeOutputEvent
+from dynamic_system.input_bag import InputBag
 
 if TYPE_CHECKING:
     from dynamic_system.atomic_models.bag_of_values import BagOfValues
+
 
 
 @subscriber
@@ -15,9 +17,7 @@ class InputModel(BaseModel):
     _inputs: BagOfValues
 
     @subscribe(ComputeOutputEvent)
-    def compute_output(self) -> BagOfValues:
+    def receive_input(self, input_bag: InputBag):
         out = self.output_function(self._inputs)
-        return out
-
-    def output_function(self, inputs: BagOfValues) -> BagOfValues:
-        return inputs
+        for model in self._listeners:
+            model.receive_input(InputBag(self, out))
