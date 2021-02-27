4371d59041386de62e8751319d86398d3379e150
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-02-27 15:18:50 -0400

    Documentation

--- src/dynamic_system/base_model.py
+++ src/dynamic_system/base_model.py
@@ -7,7 +7,10 @@ if TYPE_CHECKING:
 
 from abc import abstractmethod
 
+
 class BaseModel:
+    """"Model in a dynamic system
+    """
     _id: int
     _serial_id = 0
 
@@ -18,22 +21,36 @@ class BaseModel:
     _listeners: List[BaseModel]
 
     def _add_listener(self, model: BaseModel):
+        """Add a model to listeners
+        :param model Model that will be listen
+        """
         self._listeners.append(model)
 
     def _remove_listener(self, model: BaseModel):
+        """Remove a model from listeners
+        :param model Model to be removed
+        """
         self._listeners.remove(model)
 
-    def get_id(self):
+    def get_id(self) -> int:
+        """Returns the identifier of the model"""
         return self._id
 
     def notify_output(self, out: BagOfValues):
+        """Notifies to listeners the arrival of an output
+        :param out Output that will be an input for a listener"""
         for model in self._listeners:
             model.receive_input(self.get_id(), out)
 
     @abstractmethod
     def receive_input(self, model_id: int, inputs: BagOfValues):
+        """Receive the output from a model that is an input for the current model
+        :param model_id ID of the model that emits the input
+        :param inputs Output of the model that serves of input for the current model
+        """
         pass
 
     def summary(self):
+        """Print a summary of the model"""
         row_format = "{:>15}" * 5
         print(row_format.format("", *["ID", "TYPE", "NAME", "INPUTS", "OUTPUTS"]))
--- src/dynamic_system/scheduler.py
+++ src/dynamic_system/scheduler.py
@@ -8,22 +8,32 @@ if TYPE_CHECKING:
 
 
 class Scheduler:
+    """Scheduler for execution of the autonomous events of discrete-event models"""
     _schedule: List[ScheduledModel]
 
     def __init__(self):
         self._schedule = []
 
     def schedule(self, model: Model, time: float):
-        sm = ScheduledModel(model, time)
-        heapq.heappush(self._schedule, sm)
+        """Schedule an event at the specified time
+        :param model Model with an autonomous event scheduled
+        :param time Time to execute event
+        """
+        if time > 0:
+            sm = ScheduledModel(model, time)
+            heapq.heappush(self._schedule, sm)
 
-    def next_time(self):
+    def next_time(self) -> float:
+        """Get time of the next event"""
         return self._schedule[0].get_time()
 
-    def update_time(self, time: float):
+    def update_time(self, delta_time: float):
+        """Update the time of the events
+        :param delta_time Time that has passed since the last update"""
         for i in range(len(self._schedule)):
-            self._schedule[i].decrease_time(time)
+            self._schedule[i].decrease_time(delta_time)
 
     def get_minimum(self) -> Model:
+        """Get the next model that will execute an autonomous event"""
         if len(self._schedule) > 0:
             return heapq.heappop(self._schedule).get_model()
