e1357f97eb6a2b7d676bada0b91e3b427d629ea5
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-05-03 17:46:29 -0400

    StateModel documentation

--- src/dynamic_system/models/state_model.py
+++ src/dynamic_system/models/state_model.py
@@ -8,10 +8,17 @@ from dynamic_system.models.dynamic_system import DynamicSystem
 
 
 class StateModel(BaseModel):
+    """Model with an state"""
     _currentState: Any
     _currentDynamicSystem: DynamicSystem
 
     def __init__(self, dynamic_system: DynamicSystem, name: str = None, state=None):
+        """
+        Args:
+            dynamic_system (DynamicSystem): Dynamic system of the model.
+            name (str): Name of the model.
+            state (Any): Initial state of the model.
+        """
         super().__init__(name)
         # Init the model
         self.setUpState(state)
@@ -20,19 +27,19 @@ class StateModel(BaseModel):
         self._currentDynamicSystem.add(self)
 
     def add(self, model: StateModel):
-        """Add a model as an input for the current model in the dynamic system
-        :param model: Model to be an input"""
+        """Adds a model as an input for the current model in the dynamic system.
+
+        Args:
+            model (StateModel):Model to be an input.
+        """
         self._currentDynamicSystem.addInput(self, model)
 
-    def getDynamicSystem(self):
+    def getDynamicSystem(self) -> DynamicSystem:
         """Returns the dynamic system where the current model belongs with"""
         return self._currentDynamicSystem
 
     def getOutput(self) -> Any:
-        """Get the output of the model.
-
-        :returns y: output trajectory y.
-        """
+        """Gets the output of the model."""
         return self.outputFunction(self._currentState)
 
     def setUpState(self, state: Any):
@@ -40,28 +47,34 @@ class StateModel(BaseModel):
 
         Sets up the state of the model.
 
-        :param state: New state of the model.
+        Args:
+            state (Any): New state of the model.
         """
         self._currentState = state
 
     @abstractmethod
     def outputFunction(self, state: Any) -> Any:
-        """.. math:: \lambda \; (s)
+        """
+        .. math:: \lambda \; (s)
 
         Implements the output function lambda. The output function describes
         how the state of the system appears to an observer.
 
         .. math:: \lambda \; : \; S \; \longrightarrow Y
 
-        :param state: current state s of the model.
-
-        :returns y: output trajectory y.
+        Args:
+            state (Any): Current state s of the model.
         """
         pass
 
     @abstractmethod
     def stateTransition(self, *args, **kwargs):
-        """Executes the state transition."""
+        """Executes the state transition.
+
+        Args:
+            *args:
+            **kwargs:
+        """
         pass
 
     def getTime(self):
