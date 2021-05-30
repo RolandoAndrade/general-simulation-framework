dc00dd0259bc4afffb10cab0f0da779f82259d54
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-05-30 13:43:52 -0400

    Started sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-
+sys.path.insert(0, os.path.abspath('../src'))
 
 # -- Project information -----------------------------------------------------
 
@@ -56,12 +56,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-autoapi_python_use_implicit_namespaces = True
+
 # -- Extension configuration -------------------------------------------------
+imgmath_image_format = 'svg'
+
+imgmath_latex = 'latex'
--- src/core/modules/base_module.py
+++ src/core/modules/base_module.py
@@ -1,6 +1,3 @@
-from injector import Module
-
-
-class BaseModule(Module):
+class BaseModule():
     def __init__(self):
         print("Init module " + str(self.__class__.__name__))
