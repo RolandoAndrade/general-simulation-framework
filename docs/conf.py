b95d074ffc885cc6e2558f92b08a86e2834f0970
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-05-30 12:47:37 -0400

    Started sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,17 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+
+# sphinx.ext.mathbase
+# sphinx.ext.pngmath
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.napoleon'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -38,7 +48,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -52,3 +62,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autoapi_python_use_implicit_namespaces = True
+# -- Extension configuration -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,14 +1,16 @@
+.. General Simulation Framework documentation master file, created by
+   sphinx-quickstart on Sun May 30 11:37:45 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
 Welcome to General Simulation Framework's documentation!
 ========================================================
-
-.. automodule:: my_project.main
-    :members:
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-
+   src/modules
 
 Indices and tables
 ==================
@@ -16,3 +18,4 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
+
