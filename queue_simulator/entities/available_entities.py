dcd96605d298bcd1797fddc0dbf463de506c1a0a
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-09-14 11:23:55 -0400

    Server properties

--- queue_simulator/entities/available_entities.py
+++ queue_simulator/entities/available_entities.py
@@ -4,7 +4,7 @@ import enum
 class AvailableEntities(str, enum.Enum):
     SOURCE = "SOURCE"
     SERVER = "SERVER"
-    ENTITY_EMITTER = "ENTITY_EMITTER"
+    ENTITY_EMITTER = "EMITTER"
     SINK = "SINK"
     PATH = "PATH"
     ENTITY = "ENTITY"
--- queue_simulator/shared/nodes/node_types.py
+++ queue_simulator/shared/nodes/node_types.py
@@ -4,5 +4,5 @@ import enum
 class NodeType(str, enum.Enum):
     SOURCE = "SOURCE"
     SERVER = "SERVER"
-    ENTITY_EMITTER = "ENTITY_EMITTER"
+    ENTITY_EMITTER = "EMITTER"
     SINK = "SINK"
