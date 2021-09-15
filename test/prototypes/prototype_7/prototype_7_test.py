a563433d6c30f390b97a7c9077da4301c1d5912e
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-09-15 12:16:38 -0400

    Prototype 7 tests

--- test/prototypes/prototype_7/prototype_7_test.py
+++ test/prototypes/prototype_7/prototype_7_test.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 
 from core.entity.core import static_entity_manager
 from core.entity.properties import NumberProperty
-from core.mathematics.distributions import TriangularDistribution
+from core.mathematics.distributions import TriangularDistribution, ExponentialDistribution, PoissonDistribution
 from core.mathematics.values.value import Value
 from core.types import Time
 from experiments.experiment_builders import DiscreteEventExperiment
@@ -38,6 +38,24 @@ class Prototype6Test(unittest.TestCase):
             'time': Time(50),
             'inter_arrival_time': Value(1),
             'entities_per_arrival': Value(1),
+            'time_offset': Value(0),
+            'processing_time': TriangularDistribution(2, 5, 10),
+            'initial_capacity': NumberProperty(1000),
+            'expected': 45
+        },
+        'test_4': {
+            'time': Time(50),
+            'inter_arrival_time': ExponentialDistribution(0.25),
+            'entities_per_arrival': Value(1),
+            'time_offset': Value(0),
+            'processing_time': Value(2),
+            'initial_capacity': NumberProperty(1000),
+            'expected': 45
+        },
+        'test_5': {
+            'time': Time(50),
+            'inter_arrival_time': ExponentialDistribution(0.25),
+            'entities_per_arrival': PoissonDistribution(5),
             'time_offset': Value(0),
             'processing_time': TriangularDistribution(2, 5, 10),
             'initial_capacity': NumberProperty(1000),
@@ -98,22 +116,22 @@ class Prototype6Test(unittest.TestCase):
         time = self.vectors[test]['time']
         self.source.inter_arrival_time = self.vectors[test]['inter_arrival_time']
         self.source.entities_per_arrival = self.vectors[test]['entities_per_arrival']
-        self.source.time_offset = self.vectors[test]['time_offset']
+        self.server.processing_time = self.vectors[test]['processing_time']
+        self.server.initial_capacity = self.vectors[test]['initial_capacity']
         self.experiment.simulation_control.start(stop_time=time)
         self.experiment.simulation_control.wait()
-        self.assertEqual(self.vectors[test]['expected'],
-                         self.source.get_state().output_buffer.number_entered.get_value())
+        print(self.server.get_state().output_buffer.number_entered.get_value())
 
     def test_source_validation_5(self):
         test = 'test_5'
         time = self.vectors[test]['time']
         self.source.inter_arrival_time = self.vectors[test]['inter_arrival_time']
         self.source.entities_per_arrival = self.vectors[test]['entities_per_arrival']
-        self.source.time_offset = self.vectors[test]['time_offset']
+        self.server.processing_time = self.vectors[test]['processing_time']
+        self.server.initial_capacity = self.vectors[test]['initial_capacity']
         self.experiment.simulation_control.start(stop_time=time)
         self.experiment.simulation_control.wait()
-        self.assertEqual(self.vectors[test]['expected'],
-                         self.source.get_state().output_buffer.number_entered.get_value())
+        print(self.server.get_state().output_buffer.number_entered.get_value())
 
 
 if __name__ == "__main__":
