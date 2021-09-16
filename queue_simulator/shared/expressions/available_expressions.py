b49680ed9e38b361afd17f53cb70e4cfd38de651
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-09-16 08:50:25 -0400

    Starting expression manager

--- queue_simulator/shared/expressions/available_expressions.py
+++ queue_simulator/shared/expressions/available_expressions.py
@@ -1,45 +1,63 @@
 from __future__ import annotations
 
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 from core.expresions import Expression, UserExpression
-import core.mathematics.distributions as Random
+from core.mathematics.distributions import ExponentialDistribution, PoissonDistribution, TriangularDistribution
 
 
 class ExpressionManager:
-    available_expressions = {
-        'Random': {
-            'ExponentialDistribution': {
-                'value': 'Random.ExponentialDistribution',
-                'params': ['mean']
-            },
-            'PoissonDistribution': {
-                'value': 'Random.PoissonDistribution',
-                'params': ['mean']
-            },
-            'TriangularDistribution': {
-                'value': 'Random.TriangularDistribution',
-                'params': ['minimum', 'mode', 'maximum']
-            },
+    _available_expressions: Dict[str, Any]
+
+    def __init__(self):
+        self._available_expressions = {
+            'Random': {
+                'Exponential': {
+                    'value': 'Random.Exponential',
+                    'class': ExponentialDistribution,
+                    'params': ['mean']
+                },
+                'Poisson': {
+                    'value': 'Random.Poisson',
+                    'class': PoissonDistribution,
+                    'params': ['mean']
+                },
+                'Triangular': {
+                    'value': 'Random.Triangular',
+                    'class': TriangularDistribution,
+                    'params': ['minimum', 'mode', 'maximum']
+                },
+            }
         }
-    }
 
     @staticmethod
-    def get_expression(value: str, options=None) -> Expression:
+    def _extract_params(value: str) -> List[float]:
+        s = value[value.find("(") + 1:value.find(")")]
+        return [float(param) for param in s.split(',')]
+
+    def get_expression(self, value: str, params: List[float] = None, options=None) -> Expression:
         try:
             return UserExpression(eval(value))
         except Exception:
             if options is None:
-                options = ExpressionManager.available_expressions
+                options = self._available_expressions
+                params = params or ExpressionManager._extract_params(value)
+                value = value[:value.find("(")]
+
+            if "value" in options and isinstance(options['value'], str):
+                if 'class' in options:
+                    return options['class'](*params)
+                if 'expression' in options:
+                    return options['expression'].evaluate()
             key, _, next_keys = value.partition('.')
-
             if key in options:
-                return ExpressionManager.get_expression(next_keys, options[key])
+                return self.get_expression(next_keys, params, options[key])
 
-    @staticmethod
-    def add_expression(value: str, options: Dict[str, Any]):
-        ExpressionManager.available_expressions[value] = options
+    def add_expression(self, value: str, options: Dict[str, Any]):
+        self._available_expressions[value] = options
 
-    @staticmethod
-    def remove_expression(value: str):
-        ExpressionManager.available_expressions[value] = options
+    def remove_expression(self, value: str):
+        self._available_expressions.pop(value)
+
+    def get_available_expressions(self):
+        return self._available_expressions
--- test/queue_simulator/expressions/expressions_test.py
+++ test/queue_simulator/expressions/expressions_test.py
@@ -1,9 +1,28 @@
 import unittest
 
+from core.mathematics.distributions import ExponentialDistribution, PoissonDistribution, TriangularDistribution
+from queue_simulator.shared.expressions.available_expressions import ExpressionManager
+
 
 class ExpressionsTest(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def setUp(self) -> None:
+        self.manager = ExpressionManager()
+
+    def test_expression_get_user_expression(self):
+        expr = self.manager.get_expression("1==1")
+        self.assertEqual(True, expr.evaluate())
+
+    def test_expression_get_predefined_expression(self):
+        expr = self.manager.get_expression("Random.Exponential(0.25)")
+        self.assertIsInstance(expr, ExponentialDistribution)
+        expr = self.manager.get_expression("Random.Poisson(5)")
+        self.assertIsInstance(expr, PoissonDistribution)
+        expr = self.manager.get_expression("Random.Triangular(0,5,10)")
+        self.assertIsInstance(expr, TriangularDistribution)
+
+    def test_expression_get_available_expressions(self):
+        d = self.manager.get_available_expressions()
+        self.assertDictEqual(self.manager._available_expressions, d)
 
 
 if __name__ == '__main__':
