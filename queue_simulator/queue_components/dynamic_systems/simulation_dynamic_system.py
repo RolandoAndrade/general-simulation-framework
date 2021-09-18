6d94730ed4307f0f57ebd9414cd6db289db754a4
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-09-18 09:48:01 -0400

    Routing fix

--- queue_simulator/queue_components/dynamic_systems/simulation_dynamic_system.py
+++ queue_simulator/queue_components/dynamic_systems/simulation_dynamic_system.py
@@ -1,17 +1,20 @@
 from __future__ import annotations
 
-from typing import List, Union, Set
+from typing import List, Union, Set, Dict
 
 from dynamic_system.dynamic_systems import DiscreteEventDynamicSystem
 from dynamic_system.future_event_list import Scheduler
+from models.models import DiscreteEventModel
+from queue_simulator.queue_components.route import Route
 from queue_simulator.queue_components.server import Server
 from queue_simulator.queue_components.shared.stats import ComponentStats
 from queue_simulator.queue_components.sink.sink import Sink
 from queue_simulator.queue_components.source import Source
-
+import numpy as np
 
 class SimulationDynamicSystem(DiscreteEventDynamicSystem):
     _models: Set[Union[Server, Source, Sink]]
+    _paths: Dict[DiscreteEventModel, Set[Route]]
 
     def __init__(self, scheduler=None):
         """Constructs the dynamic system"""
@@ -41,3 +44,17 @@ class SimulationDynamicSystem(DiscreteEventDynamicSystem):
         for model in self._models:
             s.append(model.get_stats())
         return s
+
+    def _get_effective_paths(self, emitter_model: DiscreteEventModel) -> Set[Route]:
+        """Gets the correct paths for an output"""
+        if emitter_model in self._paths:
+            weights = []
+            effective_path = []
+            for path in self._paths[emitter_model]:
+                weights.append(path.get_weight())
+                effective_path.append(path)
+            if len(weights) > 0:
+                weights = np.array(weights) / sum(weights)
+                choice = np.random.choice(len(weights), p=weights)
+                return {effective_path[choice]}
+        return set()
